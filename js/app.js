f9a07c82608c65be917e10860648bf626ddce370
Ross Wu <RAWSE-DA-BAWSE@Rosss-MacBook-Pro.local>
2017-04-22 18:45:10 -1000

    8 correct?

--- js/app.js
+++ js/app.js
@@ -7,34 +7,69 @@ The function will add the following content inside the paragraph with the id of 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
 
+function moreContent(){
+  more.innerHTML = bacon;
+}
+
+
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
+
+function lessContent(){
+  less.innerHTML = less.style.display ="none";
+}
+
 
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
 
+function zoom(){
+  biggie.style.fontSize="150%";
+}
+
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+var menuItems = ["McChicken", "McDouble", "Large Drink"];
+
+function valueMenu(){
+menu.innerHTML = menuItems;
+
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 20px after clicking on the text.*/
-
+function redFace(){
+  drink.style.fontSize = "20px";
+  drink.style.color = "red";
+}
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+function showPrice(){
+  price.innerHTML = "$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
+
+Benjamin.addEventListener("click", display);
+
+function display(){
+displayQuote.innerHTML = myQuote;
+
+
+
+}
 
 
 //8. Say It again, Randomly
@@ -42,8 +77,28 @@ var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+random.addEventListener("click", daQuotes);
+
+var randomQuotes = quotes[Math.floor(Math.random() * quotes.length) ]; // math floor rounds to nearest interger
+                                                                      // math random randomize between 0,1
+function daQuotes(){
+  displayQuotes.innerHTML = randomQuotes;
+}
+
 
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clickig on the button.
 */
+
+showHide.addEventListener("click", aloha);
+
+
+function aloha (){
+
+  if(showmoney.style.display === "none"){
+    showmoney.style.display ="block"; 
+  }else{
+    showmoney.style.display ="none";
+  }
+}
